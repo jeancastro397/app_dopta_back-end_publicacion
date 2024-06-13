900e617159b8f51c9ce7d22b7fe03ee8531bf5f5
Jean Castro <je.castrob@duocuc.cl>
2024-06-13 19:39:40 -0400

    modificacion del sistema de reportes en su totalidad

--- reportes/admin.py
+++ reportes/admin.py
@@ -1,3 +1,33 @@
 from django.contrib import admin
+from .models import ReporteMascota, ReporteEvento, ReporteServicio
 
-# Register your models here.
+class ReporteMascotaAdmin(admin.ModelAdmin):
+    list_display = ('usuario', 'mascota', 'motivo', 'descripcion', 'estado', 'fecha_reporte')
+    readonly_fields = ('usuario', 'mascota', 'motivo', 'descripcion', 'fecha_reporte')
+    
+    def get_readonly_fields(self, request, obj=None):
+        if not request.user.is_superuser:
+            return self.readonly_fields + ('estado',)
+        return self.readonly_fields
+
+class ReporteEventoAdmin(admin.ModelAdmin):
+    list_display = ('usuario', 'evento', 'motivo', 'descripcion', 'estado', 'fecha_reporte')
+    readonly_fields = ('usuario', 'evento', 'motivo', 'descripcion', 'fecha_reporte')
+    
+    def get_readonly_fields(self, request, obj=None):
+        if not request.user.is_superuser:
+            return self.readonly_fields + ('estado',)
+        return self.readonly_fields
+
+class ReporteServicioAdmin(admin.ModelAdmin):
+    list_display = ('usuario', 'servicio', 'motivo', 'descripcion', 'estado', 'fecha_reporte')
+    readonly_fields = ('usuario', 'servicio', 'motivo', 'descripcion', 'fecha_reporte')
+    
+    def get_readonly_fields(self, request, obj=None):
+        if not request.user.is_superuser:
+            return self.readonly_fields + ('estado',)
+        return self.readonly_fields
+
+admin.site.register(ReporteMascota, ReporteMascotaAdmin)
+admin.site.register(ReporteEvento, ReporteEventoAdmin)
+admin.site.register(ReporteServicio, ReporteServicioAdmin)
--- reportes/views.py
+++ reportes/views.py
@@ -1,41 +1,173 @@
 from django.shortcuts import get_object_or_404
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import status, generics
 from rest_framework.views import APIView
 from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from rest_framework.response import Response
-from rest_framework import status, generics
-from publicaciones.models import Mascota
-from .models import Reporte
-from .serializers import ReporteSerializer
-from django.contrib.contenttypes.models import ContentType
+from .models import (
+    ReporteMascota,
+    ReporteEvento,
+    ReporteServicio
+)
+from .serializers import (
+    ReporteMascotaSerializer,
+    ReporteEventoSerializer,
+    ReporteServicioSerializer
+)
+from publicaciones.models import (
+    Mascota,
+    Evento,
+    Servicio
+)
+from rest_framework.serializers import Serializer
+from itertools import chain
 
 
-### CRUD DE REPORTES
-# Agregar un reporte (reportar una publicacion)
-class Reportar(APIView):
+## CRUD DE REPORTES DE MASCOTA
+# REPORTAR MASCOTA
+class ReportarMascota(APIView):
     permission_classes = [IsAuthenticated]
 
     def post(self, request, pk):
-        serializer = ReporteSerializer(data=request.data)
-        if serializer.is_valid():
-            content_type = serializer.validated_data.get('content_type')
-            content_type = ContentType.objects.get(model=content_type)
-            model_class = content_type.model_class()
-            content_object = get_object_or_404(model_class, pk=pk)
+        try:
+            mascota = get_object_or_404(Mascota, pk=pk)
+            reporte, created = ReporteMascota.objects.get_or_create(usuario=request.user, mascota=mascota)
 
-            Reporte.objects.create(
-                usuario=request.user,
-                content_object=content_object,
-                motivo=serializer.validated_data.get('motivo'),
-                descripcion=serializer.validated_data.get('descripcion')
-            )
-            return Response({"message": "Publicación reportada con éxito"}, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+            if created:
+                return Response({"message": "Publicación de mascota reportada."}, status=status.HTTP_201_CREATED)
+            else:
+                return Response({"message": "La publicación ya se encuentra reportada por este usuario"}, status=status.HTTP_200_OK)
+
+        except Exception as e:
+            return Response({"message": str(e)}, status=status.HTTP_400_BAD_REQUEST)
 
 
+# Remover la publicacion de los reportes (Tabla Reporte, no Mascota)
+class RemoveReporteMascota(APIView):
+    permission_classes = [IsAuthenticated]
 
-# Listar reportes mandados, sólo un  usuario administrador puede listarlos
-class Reportes(generics.ListAPIView):
-    permission_classes = [IsAdminUser]
+    def delete(self, request, pk):
+        mascota = get_object_or_404(Mascota, pk=pk)
+        reporte = get_object_or_404(ReporteMascota, usuario=request.user, mascota=mascota)
+        reporte.delete()
 
-    queryset = Reporte.objects.all()
-    serializer_class = ReporteSerializer
+        return Response({"message": "El reporte a sido revocado."}, status=status.HTTP_204_NO_CONTENT)
+
+
+
+## CRUD DE REPORTES DE EVENTO
+# REPORTAR EVENTO
+class ReportarEvento(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request, pk):
+        try:
+            evento = get_object_or_404(Evento, pk=pk)
+            reporte, created = ReporteEvento.objects.get_or_create(usuario=request.user, evento=evento)
+
+            if created:
+                return Response({"message": "Publicación de evento reportada."}, status=status.HTTP_201_CREATED)
+            else:
+                return Response({"message": "La publicación ya se encuentra reportada por este usuario"}, status=status.HTTP_200_OK)
+
+        except Exception as e:
+            return Response({"message": str(e)}, status=status.HTTP_400_BAD_REQUEST)
+
+
+# Remover la publicacion de los reportes
+class RemoveReporteEvento(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def delete(self, request, pk):
+        evento = get_object_or_404(Evento, pk=pk)
+        reporte = get_object_or_404(ReporteEvento, usuario=request.user, evento=evento)
+        reporte.delete()
+
+        return Response({"message": "El reporte a sido revocado."}, status=status.HTTP_204_NO_CONTENT)
+
+
+
+## CRUD DE REPORTES DE SERVICIOS
+# REPORTAR SERVICIO
+class ReportarServicio(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request, pk):
+        try:
+            servicio = get_object_or_404(Servicio, pk=pk)
+            reporte, created = ReporteServicio.objects.get_or_create(usuario=request.user, servicio=servicio)
+
+            if created:
+                return Response({"message": "Publicación de servicio reportada."}, status=status.HTTP_201_CREATED)
+            else:
+                return Response({"message": "La publicación ya se encuentra reportada por este usuario"}, status=status.HTTP_200_OK)
+
+        except Exception as e:
+            return Response({"message": str(e)}, status=status.HTTP_400_BAD_REQUEST)
+
+
+# Remover la publicacion de los reportes
+class RemoveReporteServicio(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def delete(self, request, pk):
+        servicio = get_object_or_404(Servicio, pk=pk)
+        reporte = get_object_or_404(ReporteServicio, usuario=request.user, servicio=servicio)
+        reporte.delete()
+
+        return Response({"message": "El reporte a sido revocado."}, status=status.HTTP_204_NO_CONTENT)
+
+
+
+# Listar reportes del propio usuario
+class ListReportesUsuario(generics.ListAPIView):
+    permission_classes = [IsAuthenticated]
+
+    # Combina los serializadores de las publicaciones
+    def get_serializer_class(self):
+        class CombinedSerializer(Serializer):
+            reportes_mascota = ReporteMascotaSerializer(many=True)
+            reportes_evento = ReporteEventoSerializer(many=True)
+            reportes_sericio = ReporteServicioSerializer(many=True)
+
+        return CombinedSerializer
+
+    # Retorna la lista de reportes del usuario para cualquier publicacion reportada
+    def get_queryset(self):
+        reportes_mascota = ReporteMascota.objects.filter(usuario=self.request.user)
+        reportes_evento = ReporteEvento.objects.filter(usuario=self.request.user)
+        reportes_servicio = ReporteServicio.objects.filter(usuario=self.request.user)
+
+        combined_querysets = list(chain(reportes_mascota, reportes_evento, reportes_servicio))
+        
+        return combined_querysets
+
+
+# Listar reportes de todos los usuarios (solo admin)
+class ListReportesAdmin(generics.ListAPIView):
+    permission_classes = [IsAuthenticated, IsAdminUser]
+    filter_backends = [DjangoFilterBackend]
+
+    filterset_fields = {
+        'usuario__username': ['contains'],
+        'motivo': ['contains'],
+        'fecha_reporte': ['contains'],
+        'estado': ['contains'],
+    }
+
+    def get_serializer_class(self):
+        class CombinedSerializer(Serializer):
+            reportes_mascota = ReporteMascotaSerializer(many=True)
+            reportes_evento = ReporteEventoSerializer(many=True)
+            reportes_servicio = ReporteServicioSerializer(many=True)
+
+        return CombinedSerializer
+
+    def get_queryset(self):
+        reportes_mascota = ReporteMascota.objects.all()
+        reportes_evento = ReporteEvento.objects.all()
+        reportes_servicio = ReporteServicio.objects.all()
+
+        combined_querysets = list(chain(reportes_mascota, reportes_evento, reportes_servicio))
+
+        return combined_querysets
